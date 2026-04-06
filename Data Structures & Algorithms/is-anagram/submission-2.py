7053e54c56e3c185ec556abd5a5a172c57822c38
Mohamed Nasser <mohamed.n.allam@proton.me>
2026-04-06 09:54:19 +0200

    Update: is-anagram - submission-2

--- Data Structures & Algorithms/is-anagram/submission-2.py
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -1,5 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = ''.join(sorted(s))
-        t = ''.join(sorted(t))
-        return s == t
+        if len(s) != len(t):
+            return False
+        
+        # Checking frequency is O(n), while sorting is O(nlogn) 
+        s_freq = [0]*26
+        t_freq = [0]*26
+
+        for c in s:
+            s_freq[ord(c) - ord('a')] += 1
+        for c in t:
+            t_freq[ord(c) - ord('a')] += 1
+    
+        return s_freq == t_freq
